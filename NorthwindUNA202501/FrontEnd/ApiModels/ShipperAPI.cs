7b55e422b08776a4e69e2c9c6136fd1202e2565b
N3TWORKL3G3ND <rojascastrojavier13@gmail.com>
2025-03-14 20:15:59 -0600

    Progreso en frontend

---
+++ NorthwindUNA202501/FrontEnd/ApiModels/ShipperAPI.cs
@@ -0,0 +1,9 @@
+﻿namespace FrontEnd.ApiModels
+{
+    public class ShipperAPI
+    {
+        public int ShipperId { get; set; }
+        public string CompanyName { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ NorthwindUNA202501/FrontEnd/Helpers/Implementations/ShipperHelper.cs
@@ -0,0 +1,64 @@
+﻿using FrontEnd.ApiModels;
+using FrontEnd.Helpers.Interfaces;
+using FrontEnd.Models;
+
+namespace FrontEnd.Helpers.Implementations
+{
+    public class ShipperHelper : IShipperHelper
+    {
+        IServiceHelper _helper;
+
+        public ShipperHelper(IServiceHelper helper)
+        {
+            _helper = helper;
+        }
+
+        ShipperViewModel Convertir(ShipperAPI shipper)
+        {
+            return new ShipperViewModel
+            {
+                ShipperId = shipper.ShipperId,
+                CompanyName = shipper.CompanyName,
+                Phone = shipper.Phone
+            };
+        }
+
+        CategoryAPI Convertir(CategoryViewModel category)
+        {
+            return new CategoryAPI
+            {
+                CategoryId = category.CategoryId,
+
+                CategoryName = category.CategoryName
+            };
+        }
+
+
+
+
+        public ShipperViewModel Create(ShipperViewModel shipper)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ShipperViewModel Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ShipperViewModel> GetShippers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ShipperViewModel Update(ShipperViewModel Shipper)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
